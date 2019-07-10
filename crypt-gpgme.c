23c5d9079ea2ec4ca7c543a2fd6ef0ad465c3b08
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2019-07-10 13:38:07 +0200

    Fix generating hints with e-mail addresses
    Before this change mutt incorrectly inserted e-mail addresses into list
    of hints. For example e-mail "john@example.com" was inserted as
    "john@example" thus suggesting e-mail addresses from "example"
    subdomain.
    
    The fix removes dot (".") from tokenizer strings preserving entire
    domain name from the e-mail.

--- crypt-gpgme.c
+++ crypt-gpgme.c
@@ -4357,8 +4357,8 @@ static LIST *crypt_add_string_to_hints (LIST *hints, const char *str)
   if ((scratch = safe_strdup (str)) == NULL)
     return hints;
 
-  for (t = strtok (scratch, " ,.:\"()<>\n"); t;
-       t = strtok (NULL, " ,.:\"()<>\n"))
+  for (t = strtok (scratch, " ,:\"()<>\n"); t;
+       t = strtok (NULL, " ,:\"()<>\n"))
     {
       if (strlen (t) > 3)
         hints = mutt_add_list (hints, t);
